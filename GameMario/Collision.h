cd8aa2a24fdace481316097a4e07a49610c56b32
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-04-21 19:06:38 +0700

    fixed tailwhip sweptaabb behaviour

--- GameMario/Collision.h
+++ GameMario/Collision.h
@@ -62,6 +62,22 @@ public:
 		float& nx,
 		float& ny);
 
+	static void SweptAABB(
+		float ml,			// move left 
+		float mt,			// move top
+		float mr,			// move right 
+		float mb,			// move bottom
+		float dx,			// 
+		float dy,			// 
+		float sl,			// static left
+		float st,
+		float sr,
+		float sb,
+		float& t,
+		float& nx,
+		float& ny,
+		LPGAMEOBJECT objSrc);
+
 	LPCOLLISIONEVENT SweptAABB(
 		LPGAMEOBJECT objSrc, 
 		DWORD dt,
